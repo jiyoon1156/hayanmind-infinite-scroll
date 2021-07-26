a055b7f556d2faa8f4d6d325c9de1b64b39d8efb
jiyoonhur <jiyoon1156@gmail.com>
2021-07-26 23:42:21 +0900

    [CHORE] Linter fix
    - Import React in all files
    - Remove unnecessary file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import InfiniteScrollList from './components/InfiniteScrollList';
+import React from 'react';
+// import InfiniteScrollList from './components/InfiniteScrollList';
 import GlobalStyle from './GlobalStyle';
+import CommentCard from './components/CommentCard';
 
 const App = () => {
-	return (
-		<>
-			<GlobalStyle />
-			<InfiniteScrollList />
-		</>
-	);
+  return (
+    <>
+      <GlobalStyle />
+      {/* <InfiniteScrollList /> */}
+      <CommentCard />
+    </>
+  );
 };
 
 export default App;
--- src/components/InfiniteScrollList.js
+++ src/components/InfiniteScrollList.js
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import CommentCard from './CommentCard';
 
 const InfiniteScrollList = () => {
-	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/comments?_page=1&_limit=10')
-			.then((response) => response.json())
-			.then((data) => console.log(data));
-	}, []);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments?_page=1&_limit=10')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
-	console.log('infinitescrolllist');
-	return <CommentCard />;
+  console.log('infinitescrolllist');
+  return <CommentCard />;
 };
 
 export default InfiniteScrollList;
