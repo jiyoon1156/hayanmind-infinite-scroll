5110308874f0678acb04bcf6be7c3a2f451adbce
Jake Lee <jake.lee4006@gmail.com>
2021-07-26 22:27:18 +0900

    [CHORE] Basic set up for project

---
+++ src/components/CommentCard.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+
+
+const CommentCard = () => {
+
+  return <StyledCard></StyledCard>
+};
+
+const StyledCard = styled.div`
+
+`
+
+export default CommentCard;
---
+++ src/components/InfiniteScrollList.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import CommentCard from './CommentCard';
+
+const InfiniteScrollList = () => {
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/comments?_page=1&_limit=10')
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	}, []);
+
+	console.log('infinitescrolllist');
+	return <CommentCard />;
+};
+
+export default InfiniteScrollList;
